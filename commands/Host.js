c037951570743ba09fdf1c636da8e453dbeb70ec
Lucas Boinet <lucas.boinet@etu.univ-grenoble-alpes.fr>
2021-06-28 20:28:52 +0200

    host create command

--- commands/Host.js
+++ commands/Host.js
@@ -9,17 +9,61 @@ module.exports = {
 
         if(args[0] === 'create')
         {
+            let gamemode;
+            let date;
+            if(!args[1] || !args[2] || !args[3])
+            {
+                message.author.send('Verifier les arguments! Voir !host help pour plus d\'informations');
+                message.delete();
+                return;
+            }
+
+            gamemode = gamemodes.find(g => g.slug == args[1]);
+
+            if(!gamemode){
+                message.author.send(`Aucun mode de jeu correspondant à ${args[1]}! Voir !host help pour plus d'informations`);
+                message.delete();
+                return;
+            }
+
+            date = new Date(args[2].split('/').reverse().join('-') + 'T' + args[3] + ':00');
+
+            if(date === 'Invalid Date')
+            {
+                message.author.send(`Format de date incorrect! Voir !host help pour plus d'informations`);
+                message.delete();
+                return;
+            }
+
+            let month = date.getMonth() > 9 ? date.getMonth() : "0"+date.getMonth();
+            let hours = date.getHours() > 9 ? date.getHours() : "0"+date.getHours();
+            let dateMessage = `${date.getDate()}/${month}/${date.getFullYear()} à ${hours}h${date.getMinutes()}`;
+
             const embed = new MessageEmbed()
                 .setAuthor('Annonce Game', 'https://i.imgur.com/VgVtVns.png')
                 .setTitle(`Host de ${message.author.username}`)
                 .setColor("#9B59B6")
+                .addFields([
+                    { name: 'Mode de jeu : ', value: `${gamemode.icon} ${gamemode.text}` },
+                    { name: 'Date : ', value: `🕒 ${dateMessage}` },
+                    { name: 'Scénarios et règles : ', value: `📚 ${dateMessage}` },
+                    { name: 'IP du serveur : ', value: `🎮 play.hyzed.fr` },
+                    { name: 'Mumble : ', value: `☎️ 37.187.48.175:1030` },
+                    { name: '\u200B', value: '\u200B' }
+                ])
                 .setThumbnail('https://i.imgur.com/VgVtVns.png')
                 .setFooter('IP: play.hyzed.fr | Mumble: /mumble')
             
-            message.channel.send(embed);
+            message.channel.send(embed).then(msg => {
+                msg.react('✅');
+                msg.react('❓');
+                msg.react('❌');
+            });
             let alert_role = message.channel.guild.roles.cache.find(role => role.name === `${gamemodes[0].icon} Alerts`);
             message.channel.send(`${alert_role}`)
-                .then(msg => msg.delete())
+                .then(msg => {
+                    msg.delete();
+                })
                 .catch(err => console.error("host create: ", err));
             message.delete();
         }else if(args[0] === 'help')
